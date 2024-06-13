efd1ee338a09adb78a50e185b1f218838f51b87c
Nixotica <nixotica@gmail.com>
2024-06-12 23:40:00 -0700

    fixed docker image

--- lambdas/oauth_lambda/src/main.rs
+++ lambdas/oauth_lambda/src/main.rs
@@ -1,7 +1,5 @@
-use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_http::{Body, run, service_fn, tracing, Error, Request, Response};
 use serde::{Deserialize, Serialize};
-use reqwest::{header::HeaderMap, Client};
 
 #[derive(Deserialize)]
 struct QueryStringParameters {
@@ -28,63 +26,78 @@ struct ResponseBody {
     username: String,
 }
 
-async fn function_handler(event: LambdaEvent<ApiGatewayProxyRequest>) -> Result<ApiGatewayProxyResponse, Error> {
-    let client_id = std::env::var("CLIENT_ID").expect("CLIENT_ID must be set");
-    let client_secret = std::env::var("CLIENT_SECRET").expect("CLIENT_SECRET must be set");
-    let redirect_uri = std::env::var("REDIRECT_URI").expect("REDIRECT_URI must be set");
+async fn handler(event: Request) -> Result<Response<Body>, Error> {
+    // let client_id = std::env::var("CLIENT_ID").expect("CLIENT_ID must be set");
+    // let client_secret = std::env::var("CLIENT_SECRET").expect("CLIENT_SECRET must be set");
+    // let redirect_uri = std::env::var("REDIRECT_URI").expect("REDIRECT_URI must be set");
 
-    let mut code: String = "".to_owned();
-    let mut state: String = "".to_owned();
+    // println!("got event headers: ");
+    // event.headers().iter().for_each(|(k, v)| {
+    //     println!("{}: {}", k, v.to_str().unwrap());
+    // });
 
-    event.payload.query_string_parameters.iter().for_each(|(k, v)| {
-        match k {
-            "code" => code = v.to_string(),
-            "state" => state = v.to_string(),
-            _ => (),
-        }
-    });
+    tracing::info!("You're in the handler now.");
 
-    let global_id: Option<String> = if !state.is_empty() { Some(state) } else { None };
-    let client = Client::new();
-
-    // Trade auth code for token
-    let token_response: TokenResponse = client.post("https://itch.io/api/1/oauth/token")
-        .form(&[
-            ("client_id", &client_id),
-            ("client_secret", &client_secret),
-            ("code", &code),
-            ("grant_type", &"authorization_code".to_string()),
-            ("redirect_uri", &redirect_uri),
-        ])
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    // Get user ID in itch
-    let user_response: UserResponse = client.get("https://itch.io/api/1/key/me")
-        .bearer_auth(&token_response.access_token)
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    associate_user(global_id, &user_response).await;
-
-    let response_body = ResponseBody {
-        user_id: user_response.id,
-        username: user_response.username,
-    };
-
-    let response = ApiGatewayProxyResponse {
-        status_code: 200,
-        body: Some(aws_lambda_events::encodings::Body::Text(serde_json::to_string(&response_body)?)),
-        headers: HeaderMap::new(),
-        multi_value_headers: HeaderMap::new(),
-        is_base64_encoded: Some(false),
-    };
+    let response = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body("Hello OAuth!".into())
+        .map_err(Box::new)?;
 
     Ok(response)
+
+    // let mut code: String = "".to_owned();
+    // let mut state: String = "".to_owned();
+
+    // event.payload.query_string_parameters.iter().for_each(|(k, v)| {
+    //     match k {
+    //         "code" => code = v.to_string(),
+    //         "state" => state = v.to_string(),
+    //         _ => (),
+    //     }
+    // });
+
+    // let global_id: Option<String> = if !state.is_empty() { Some(state) } else { None };
+    // let client = Client::new();
+
+    // // Trade auth code for token
+    // let token_response: TokenResponse = client.post("https://itch.io/api/1/oauth/token")
+    //     .form(&[
+    //         ("client_id", &client_id),
+    //         ("client_secret", &client_secret),
+    //         ("code", &code),
+    //         ("grant_type", &"authorization_code".to_string()),
+    //         ("redirect_uri", &redirect_uri),
+    //     ])
+    //     .send()
+    //     .await?
+    //     .json()
+    //     .await?;
+
+    // // Get user ID in itch
+    // let user_response: UserResponse = client.get("https://itch.io/api/1/key/me")
+    //     .bearer_auth(&token_response.access_token)
+    //     .send()
+    //     .await?
+    //     .json()
+    //     .await?;
+
+    // associate_user(global_id, &user_response).await;
+
+    // let response_body = ResponseBody {
+    //     user_id: user_response.id,
+    //     username: user_response.username,
+    // };
+
+    // let response = ApiGatewayProxyResponse {
+    //     status_code: 200,
+    //     body: Some(aws_lambda_events::encodings::Body::Text(serde_json::to_string(&response_body)?)),
+    //     headers: HeaderMap::new(),
+    //     multi_value_headers: HeaderMap::new(),
+    //     is_base64_encoded: Some(false),
+    // };
+
+    // Ok(response)
 }
 
 async fn associate_user(global_id: Option<String>, user: &UserResponse) {
@@ -98,7 +111,8 @@ async fn associate_user(global_id: Option<String>, user: &UserResponse) {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = service_fn(function_handler);
-    lambda_runtime::run(func).await?;
-    Ok(())
+    // Enable CloudWatch error logs
+    tracing::init_default_subscriber();
+
+    run(service_fn(handler)).await
 }
